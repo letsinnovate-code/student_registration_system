ba1635f83ef7a1bfec5d513a51db329be029bbc0
letsinnovate-code <ap1106561@gmail.com>
2024-07-21 14:09:08 +0530

    new changes committed

---
+++ script.js
@@ -0,0 +1,26 @@
+
+// first we get all the major element from the html file 
+
+const leftContainer = document.querySelector(".left_container");
+
+const form = document.querySelector("#form");
+const rightContainer = document.querySelector(".right_container");
+const inputField = document.querySelector(".input_field");
+const submitButton = document.querySelector("#submit");
+
+
+// perform
+
+submitButton.addEventListener("click",addStudentList);
+
+
+function addStudentList(){
+  
+  
+}
+
+
+
+
+
+
